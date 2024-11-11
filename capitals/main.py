24c68d95c779d1b0087255bd9892f61eb3e24d02
Luis Louro <lapisdecor@gmail.com>
2024-11-11 20:59:03 +0000

    Add existing code

--- capitals/main.py
+++ capitals/main.py
@@ -3,16 +3,21 @@
 import csv
 import random
 import os
+from importlib import resources
+print(os.getcwd())
+from capitals import data
+
+file = resources.files(data) / 'capitais.csv'
 
 def run():
     capitais = {}
-    print(os.getcwd())
-    with open('capitals/capitais.csv', newline='') as csvfile:
-        capreader = csv.reader(csvfile)#, delimiter=' ', quotechar='"')
+
+    with open(file, newline='') as csvfile:
+        capreader = csv.reader(csvfile)
         for row in capreader:
             capitais[row[0]] = row[1]
 
-
+    capitais.pop("Country")
     country, capital = random.choice(list(capitais.items()))
     print("What's the capital of {}?".format(country))
     answer = input().capitalize()
